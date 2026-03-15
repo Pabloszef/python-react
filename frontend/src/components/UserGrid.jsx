08d15c93a7873ea08bd40a1e84dd110604c4ed6c
Pawel <pawel.zaremba013@gmail.com>
2026-03-15 19:17:26 +0100

    2 - Finish UI Part

---
+++ frontend/src/components/UserGrid.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Grid} from "@chakra-ui/react";
+import {USERS} from "../dummy/dummy.js";
+import UserCard from "./UserCard.jsx";
+
+const UserGrid = () => {
+    return (
+        <Grid templateColumns={{
+            base: "1fr",
+            md: "repeat(2, 1fr)",
+            lg: "repeat(3, 1fr)"
+        }}
+        gap={4}
+    >
+            {USERS.map((user) => (
+                <UserCard key={user.id} user={user}/>
+            ))}
+        </Grid>
+    )
+}
+export default UserGrid
